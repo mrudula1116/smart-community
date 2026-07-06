6deb70c10bc7ad53767e05c6c26720a5c24844ac
mrudula1116 <poremrudula@gmail.com>
2026-07-06 18:45:38 +0530

    resolved

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ def server_error(e):
 # ─── Run ───
 
 if __name__ == '__main__':
-    app.run(debug=Config.DEBUG, host='0.0.0.0', port=5000)
+    app.run(debug=Config.DEBUG, host='0.0.0.0', port=5000, use_reloader=False)
